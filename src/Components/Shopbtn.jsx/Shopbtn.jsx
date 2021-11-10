ba705cc6cf17289033acc1158757e39bcaa43681
Emmanuel <emmanuel.nuno.estrella@uabc.edu.mx>
2021-11-09 21:01:41 -0800

    update call to action btn

--- src/Components/Shopbtn.jsx/Shopbtn.jsx
+++ src/Components/Shopbtn.jsx/Shopbtn.jsx
@@ -3,7 +3,9 @@ import shopbtn from './shopbtn.css';
 const Shopbtn = () => {
   return (
     <div className='backgroundOfButton'>
-      <button className='btn btn-lg btn-dark shopBtn'>Let's evolve</button>
+      <button className='btn btn-lg btn-dark shopBtn'>
+        Solicitar servicio
+      </button>
     </div>
   );
 };
