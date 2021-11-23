71e3f1d973b6b117803c8aaa999d31558504134c
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2021-11-22 17:08:17 -0800

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import footer from './footer.css';
 const Footer = () => {
   return (
-    <div className='footer'>
+      <div className='footer'>
+          <p className='social-container'>
+              <a href="https://www.youtube.com/c/jamesqquick" className="youtube social">
+                  <FontAwesomeIcon icon={faYoutube} size="2x" />
+              </a>
+              <a href="https://www.facebook.com/learnbuildteach/" lassName="facebook social">
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </a>
+              <a href="http://www.instagram.com/larnbuildteach" className="instagram social">
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </a>
+          </p>
       <p>@copyright Jelpit.com.mx</p>
     </div>
   );
