20540d01e9f4d02a14f6e2ff855fa3d29e027fdc
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2022-04-08 21:35:21 -0700

    add
    update

--- src/Containers/Main/Main.jsx
+++ src/Containers/Main/Main.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import webDesign from '../../assets/image/imac-1999636_1920.png';
+import webDesign from '../../assets/image/landingImgMobile.png';
+import UtitleWebSite from '../../assets/image/Untitledwebsite.png';
 import Shopbtn from '../../Components/Shopbtn.jsx/Shopbtn';
 import mainStyle from './main.css';
 const Main = () => {
-  return (
-    <div>
-      <h1 className='titleAnimation'>Servicio Web Profesional</h1>
-      <img
+    return (
+        <div className="main-section row p-5">
+          <div className='col-xs-12 col-lg-6 p-3'>
+          <img
         src={webDesign}
         style={{ width: '100%' }}
-        className='backgroundAnimatation'
-      />
-      <Shopbtn />
+                className='backgroundAnimatation'
+                alt='tienda en linea'
+            />
+          </div>
+          <div className='col-xs-12  col-lg-6 text-centre p-3'>
+            <h1>Tu negocio digital</h1>
+            <p>Incrementa las ventas, maneja tus pedidos, hazlo mejor y mas rapido, haz lo que tu decidas, las herramientas se adaptan a tu negocio.</p>
+            <a class='btn btn-dark white-txt' href='#'>Creemos el siguiente nivel</a>
+          </div>
+     
     </div>
   );
 };
